2db670e8fb10bb308a67857ec58cf97f8b16934b
zhen77 <45620570@qq.com>
2021-01-11 19:10:47 +0800

    使用cookie 免密登陆

--- cookie/src/com/cookie/control/HelloServlet.java
+++ cookie/src/com/cookie/control/HelloServlet.java
@@ -1,30 +1,28 @@
-package com.sunzhen;
+package com.cookie.control;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class GetCookieServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //设置编码
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("gbk");
-
-        //获取cookie对象
-        Cookie[] cookies = request.getCookies();
-        if(cookies.length>0){
-            for (Cookie c: cookies) {
-                String key = c.getName();
-                String value = c.getValue();
-                System.out.println(key+":"+value);
-            }
-        }
+        String name = request.getParameter("name");
+        PrintWriter out = response.getWriter();
+        out.write("<html>");
+        out.write("<head>");
+        out.write("</head>");
+        out.write("<body>");
+        out.write("<h1>欢迎"+name+"登录<h1>");
+        out.write("</body>");
+        out.write("</html>");
     }
 }
--- cookie/web/index.jsp
+++ cookie/web/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: Sunzhen
-  Date: 2021/1/10
-  Time: 20:51
+  User: Administrator
+  Date: 2019/6/26
+  Time: 17:06
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
