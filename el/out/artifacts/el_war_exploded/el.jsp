25ad535491c84a1698225a504e0e0c20a6009c13
zhen77 <Zhen18103690519@gmail.com>
2021-01-15 15:27:02 +0800

    作用域Scope 获取作用域顺序 EL表达时进行运算

--- el/out/artifacts/el_war_exploded/el.jsp
+++ el/out/artifacts/el_war_exploded/el.jsp
@@ -1,5 +1,6 @@
 <%@ page import="com.sunzhen.entity.User" %>
-<%@ page import="java.util.ArrayList" %><%--
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="java.util.HashMap" %><%--
   Created by IntelliJ IDEA.
   User: Sunzhen
   Date: 2021/1/15
@@ -18,6 +19,53 @@ pwd:<%=request.getParameter("pwd")%>
 aa:<%=request.getAttribute("aa")%>
 town:<%=((User)request.getAttribute("user")).getAddress().getTown()%>
 香坊区:<%=((User)((ArrayList)request.getAttribute("list")).get(1)).getAddress().getTown()%>
+mapValue:<%=((HashMap)request.getAttribute("map")).get("Haerbin")%>
+名字:<%=((User)((HashMap)request.getAttribute("map2")).get("a1")).getName()%>
+
+
+<!--
+    使用传统方式获取request对象中的值有以下缺点:
+    1.必须导入包
+    2.进行类型的强制转换;
+    3.层次结构比较复杂
+
+    解决:使用El表达式;
+-->
+
+<br>
+<!--使用el表达式获取作用域的值-->
+name:${param.name}
+pwd:${param.pwd}
+aa:${aa}
+town:${user.address.town}
+香坊区:${list[1].address.town}
+mapValue:${map.Haerbin}
+名字:${map.a1.name}
+
+
+<!--使用el表达时获取对象的顺序-->
+<%
+    pageContext.setAttribute("key","Hello pageContext");
+    request.setAttribute("key","Hello request");
+    session.setAttribute("key","Hello session");
+    application.setAttribute("key","Hello application");
+%>
+<br>
+key:${key}<br>
+pageContext:${pageScope.key}
+request:${requestScope.key}
+session:${sessionScope.key}
+application:${applicationScope.key}
+
+<!--使用el表达时进行算术运算-->
+${1+1}<br>
+${2-2}<br>
+${2*2}<br>
+${2/2}<br>
+${1/0}<br>
+${1>2}<br>
+${1>2?"男":"女"}
+${1+"2"}
 
 </body>
 </html>
--- el/src/com/sunzhen/ElServlet.java
+++ el/src/com/sunzhen/ElServlet.java
@@ -11,6 +11,35 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+
+/**
+ * El表达式:
+ *      概念:
+ *          Expression Language:一种表达式语言,语法简单
+ *      作用:
+ *          方便jsp中获取作用域的属性:
+ *      语法规则:
+ *          ${expression}  可以使用.或者[]来获取属性值或者指定位置的索引
+ *          获取值的时候,直接使用作用域的中的key即可,使用.来引用属性,使用[]来获取指定索引的对象
+ *      作用域:
+ *          pageContext-->request-->session-->application
+ *          由小到大
+ *      获取作用域的顺序:
+ *          从小的作用域开始查询,如果找到返回对应的值,不断向着大范围寻找数据;
+ *          当四种作用域在相同key属性的时候,可以通过配置pageScope ,requestScope,sessionScope,applicationScope获取指定作用域的数据;
+ *       el表达时可以进行算数运算和逻辑关系运算
+ *          直接在表达式中世界写入算法操作即可,如果是关系运算,返回true或false
+ *          注意:在el表达时中"+"表示加法操作而不是字符串连接符
+ *
+ *
+ *
+ *
+ *
+ * */
 
 public class ElServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -40,6 +69,21 @@ public class ElServlet extends HttpServlet {
 
         //给request对象设置集合:
         request.setAttribute("list",list);
+
+
+        //给request对象设置map对象:
+        HashMap<String,String> map = new HashMap<>();
+        map.put("China","beijing");
+        map.put("Haerbin","xiangfang");
+
+        request.setAttribute("map",map);
+
+        HashMap<String,User> map2 = new HashMap<>();
+        map2.put("a1",user);
+        map2.put("a2",user1);
+        request.setAttribute("map2",map2);
+
+
 
         //通过某一个请求转发的方式跳转到jsp页面
         request.getRequestDispatcher("el.jsp").forward(request,response);
