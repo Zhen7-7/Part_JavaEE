587ba72b958672ed6abaee9ac4fa0cfdf8d24eaa
zhen77 <Zhen18103690519@gmail.com>
2021-01-18 10:09:57 +0800

    通过filter进行数据拦截,实现登陆

---
+++ loginFIlter/src/com/sunzhen/servlet/loginServlet.java
@@ -0,0 +1,19 @@
+package com.sunzhen.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class loginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request,response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("进入loginServlet处理过程");
+        response.getWriter().write("你好,登陆成功!");
+    }
+}
---
+++ loginFIlter/web/login.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Sunzhen
+  Date: 2021/1/18
+  Time: 9:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+请登录
+<%
+    session.setAttribute("user","user");
+%>
+</body>
+</html>
