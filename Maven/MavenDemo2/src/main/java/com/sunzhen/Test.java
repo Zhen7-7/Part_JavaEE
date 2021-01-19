dce5ba658158c6b791aeefc3656713af29784a12
zhen77 <Zhen18103690519@gmail.com>
2021-01-19 14:54:16 +0800

    Maven的依赖性 1.传递性 2.最短路径优先原则/最先声明原则 3.排除依赖

---
+++ Maven/MavenDemo2/src/main/java/com/sunzhen/Test.java
@@ -0,0 +1,10 @@
+package com.sunzhen;
+
+import org.apache.ibatis.builder.BaseBuilder;
+
+public class Test {
+    public static void main(String[] args) {
+        //BaseBuilder  说明导入进来了
+
+    }
+}
