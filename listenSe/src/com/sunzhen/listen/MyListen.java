954ed6de4f70547f56c6a6a52731bcb52d77adc1
zhen77 <Zhen18103690519@gmail.com>
2021-01-19 12:37:12 +0800

    监听器的request监听的学习

--- listenSe/src/com/sunzhen/listen/MyListen.java
+++ listenSe/src/com/sunzhen/listen/MyListen.java
@@ -2,13 +2,14 @@ package com.sunzhen.listen;
 
 import org.omg.CORBA.Request;
 
-import javax.servlet.ServletRequestAttributeEvent;
-import javax.servlet.ServletRequestAttributeListener;
-import javax.servlet.ServletRequestEvent;
-import javax.servlet.ServletRequestListener;
+import javax.servlet.*;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 import java.util.Date;
 
-public class MyListen implements ServletRequestListener , ServletRequestAttributeListener {
+public class MyListen implements ServletRequestListener , ServletRequestAttributeListener , ServletContextListener ,ServletContextAttributeListener,HttpSessionListener, HttpSessionAttributeListener {
     @Override
     public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
         System.out.println("request对象被销毁"+new Date());
@@ -23,7 +24,7 @@ public class MyListen implements ServletRequestListener , ServletRequestAttribut
     @Override
     //增加属性配置
     public void attributeAdded(ServletRequestAttributeEvent servletRequestAttributeEvent) {
-        System.out.println("向request作用域添加数据");
+        System.out.println("request作用域添加数据");
         System.out.println(servletRequestAttributeEvent.getName());
         System.out.println(servletRequestAttributeEvent.getValue());
     }
@@ -31,7 +32,7 @@ public class MyListen implements ServletRequestListener , ServletRequestAttribut
     @Override
     //删除
     public void attributeRemoved(ServletRequestAttributeEvent servletRequestAttributeEvent) {
-        System.out.println("向request作用域删除数据时");
+        System.out.println("request作用域删除数据");
         System.out.println(servletRequestAttributeEvent.getName());
         System.out.println(servletRequestAttributeEvent.getValue());
     }
@@ -41,6 +42,68 @@ public class MyListen implements ServletRequestListener , ServletRequestAttribut
         System.out.println("request作用域属性替换");
         System.out.println(servletRequestAttributeEvent.getName());
         System.out.println(servletRequestAttributeEvent.getValue());
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("Servletcontext创建");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("Servletcontext销毁");
 
     }
+
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent servletContextAttributeEvent) {
+        System.out.println("servletcontext作用域添加数据");
+        System.out.println(servletContextAttributeEvent.getName());
+        System.out.println(servletContextAttributeEvent.getValue());
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent servletContextAttributeEvent) {
+        System.out.println("servletcontext作用域移除数据");
+        System.out.println(servletContextAttributeEvent.getName());
+        System.out.println(servletContextAttributeEvent.getValue());
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent servletContextAttributeEvent) {
+        System.out.println("servletcontext作用域替换数据");
+        System.out.println(servletContextAttributeEvent.getName());
+        System.out.println(servletContextAttributeEvent.getValue());
+    }
+    @Override
+    public void sessionCreated(HttpSessionEvent httpSessionEvent) {
+        System.out.println("session对象被创建");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
+        System.out.println("session对象被销毁");
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("session添加属性");
+        System.out.println(httpSessionBindingEvent.getName());
+        System.out.println(httpSessionBindingEvent.getValue());
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("session删除属性");
+        System.out.println(httpSessionBindingEvent.getName());
+        System.out.println(httpSessionBindingEvent.getValue());
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("session修改属性");
+        System.out.println(httpSessionBindingEvent.getName());
+        System.out.println(httpSessionBindingEvent.getValue());
+    }
+
 }
--- listenSe/src/com/sunzhen/servlet/listenseServlet.java
+++ listenSe/src/com/sunzhen/servlet/listenseServlet.java
@@ -1,5 +1,6 @@
 package com.sunzhen.servlet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +19,14 @@ public class listenseServlet extends HttpServlet {
         response.setContentType("text/html;charset=utf-8");
 
         System.out.println("listenServlet逻辑处理");
-        request.setAttribute("listense","listense");
+//        request.setAttribute("listense","listense");
+//        request.removeAttribute("listense");
+        //向servletContext中添加属性值:
+        ServletContext  sc= this.getServletContext();
+        sc.setAttribute("servletcontext","servletcontext_value");
+
+
+
         response.getWriter().write("listense study");
-        request.removeAttribute("listense");
     }
 }
